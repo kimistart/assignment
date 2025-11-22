69f0a0f171e4d73d4d7e88f32cc3e6b4a5275cc7
Zhao Jingyi <zhaojingyi130@sohu.com>
2025-11-22 21:48:17 +0800

    task1andtask2

---
+++ base1/1只出现一次的数字/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func only_one(arr []int) {
+
+	m := make(map[int]int)
+
+	for _, v := range arr {
+		m[v]++
+	}
+
+	for k, v := range m {
+		if v == 1 {
+			fmt.Print(k, "只出现了一次")
+		}
+	}
+}
+
+func main() {
+	arr := []int{1, 1, 2, 2, 3, 3, 4, 4, 5}
+
+	only_one(arr)
+}
