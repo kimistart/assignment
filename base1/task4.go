8ca8cd0f719cb20b598ff354925ee38c714013bb
Zhao Jingyi <zhaojingyi130@sohu.com>
2025-11-24 17:45:35 +0800

    task1

--- base1/task4.go
+++ base1/task4.go
@@ -1,40 +1,37 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func main() {
-	strs := []string{"flower", "flow", "flight"}
-
-	fmt.Println("最长公共前缀是： ", find(strs))
-}
-
 func find(strs []string) string {
-	pub := "";
 
-	for  {
-		x := 0
-		if x <
-		for _,s := range strs {
-			if pub=="" {
-				pub += s
-				continue
-			}else {
-				chars := []rune(s)
-				subChars := chars[0:len(pub)]
-				if pub == string(subChars) {
-					x += 1
-					continue
-				}else {
-					return pub
-				}
-			}
+	//找到最短的
+	minStr := strs[0]
+	for _, s := range strs {
+		if len(s) < len(minStr) {
+			minStr = s
 		}
-		pub += s
 	}
 
+	pub := minStr
 
+	for _, s := range strs {
+		for len(pub) > 0 && !isPrefix(s, pub) {
+			pub = pub[:len(pub)-1]
+		}
+		if pub == "" {
+			break
+		}
+	}
 	return pub
 }
+
+func isPrefix(s, prefix string) bool {
+	if len(prefix) > len(s) {
+		return false
+	}
+	// 取 s 的前 len(prefix) 个字符，和 prefix 对比，相等就是前缀
+	return s[:len(prefix)] == prefix
+}
+
+/* func main() {
+	strs := []string{"flower", "flow", "flight"}
+	fmt.Println("最长公共前缀是： ", find(strs))
+} */
