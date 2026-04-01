a4e75d023b37a22e44da9f7dc5f68ccc230510fe
Zhao Jingyi <zhaojingyi130@sohu.com>
2026-04-01 20:42:03 +0800

    fix

--- solidity/task2/2_Erc721/IERC165.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
-
-interface IERC165 {
-
-    // 合约是实现了interfaceId的接口，返回true
-    function supportsInterface(bytes4 interfaceId) external view returns (bool) ;
-
-}
