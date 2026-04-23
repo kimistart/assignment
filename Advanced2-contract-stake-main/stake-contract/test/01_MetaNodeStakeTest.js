e2ebc974e9b1de8d4f43cbc455ead45bc00c3880
Zhao Jingyi <zhaojingyi130@sohu.com>
2026-04-23 17:54:30 +0800

    58.6

--- Advanced2-contract-stake-main/stake-contract/test/01_MetaNodeStakeTest.js
+++ Advanced2-contract-stake-main/stake-contract/test/01_MetaNodeStakeTest.js
@@ -39,6 +39,18 @@ describe("stake test", async function () {
         expect(poolLength).to.length.gt(0)
     })
 
+    it("getMultiplier", async () => {
+        // 当前区块高度
+        const fromBlock = await stakeProxyContract.startBlock()
+        const toBlock = fromBlock + 10n
+        const mul = await stakeProxyContract.getMultiplier(fromBlock, toBlock)
+        expect(mul).to.eq(metaNodePerBlock * (toBlock - fromBlock))
+    })
+
+    it("setMetaNodePerBlock",async () => {
+        await stakeProxyContract.connect(admin).setMetaNodePerBlock(101n)
+    })
+
     it("setMetaNode", async () => {
         const erc20 = await ethers.getContractFactory("MetaNodeToken")
         erc20Contract = await erc20.connect(admin).deploy()
@@ -98,6 +110,11 @@ describe("stake test", async function () {
         // 最小质押金额 
         const minDepositAmount = BigInt(1E18)
         const withUpdate = false
+
+        await expect(stakeProxyContract.connect(admin).addPool(
+            tokenAddress, poolWeight, minDepositAmount, 0, withUpdate)
+        ).to.be.revertedWith("invalid withdraw locked blocks")
+
         await stakeProxyContract.connect(admin).addPool(tokenAddress, poolWeight, minDepositAmount, unstakeLockedBlocks, withUpdate)
         const poolLength = await stakeProxyContract.poolLength()
         expect(poolLength).to.length.gt(1)
@@ -106,25 +123,42 @@ describe("stake test", async function () {
     it("updatePool", async () => {
         await stakeProxyContract.connect(admin).updatePool(0, 1E15, 10)
         await stakeProxyContract.connect(admin).setPoolWeight(0, 20, true)
-    })
 
-    it("getMultiplier", async () => {
-        // 当前区块高度
-        const fromBlock = await stakeProxyContract.startBlock()
-        const toBlock = fromBlock + 10n
-        const mul = await stakeProxyContract.getMultiplier(fromBlock, toBlock)
-        expect(mul).to.eq(metaNodePerBlock * (toBlock - fromBlock))
+        await expect(stakeProxyContract.connect(admin).setPoolWeight(0, 0, true)
+        ).to.be.revertedWith("invalid pool weight")
     })
 
     it("deposit", async () => {
+
+        await expect(stakeProxyContract.connect(user1).depositETH({ value: ethers.parseEther("0.000001") })
+        ).to.be.revertedWith("deposit amount is too small")
+
         // user1 deposit 10ETH, user2 deposit 20ETH
+        await provider.send("evm_setAutomine", [false])
+
         await stakeProxyContract.connect(user1).depositETH({ value: ethers.parseEther("10") })
         await stakeProxyContract.connect(user2).depositETH({ value: ethers.parseEther("20") })
+
+        await provider.send("evm_mine", [])
+        await provider.send("evm_setAutomine", [true])
+
+        await provider.send("evm_mine", [])
+        const blockNumber1 = await provider.getBlockNumber()
+        console.log("还剩余奖励(当前区块)：",blockNumber1)
+        const lastPending = await stakeProxyContract.connect(user2).pendingMetaNodeByBlockNumber(0,user2.address,blockNumber1)
+        console.log("还剩余奖励：",lastPending)
+
 
         // user3 deposit 200USD
         await erc20Contract.connect(admin).transfer(user3.address, ethers.parseEther("1000"))
         const proxyAddress = await stakeProxyContract.getAddress()
         await erc20Contract.connect(user3).approve(proxyAddress, ethers.parseEther("400"))
+
+        await expect(stakeProxyContract.connect(user3).deposit(1,ethers.parseEther("0.1"))
+        ).to.be.revertedWith("deposit amount is too small")
+        await expect(stakeProxyContract.connect(user3).deposit(0,ethers.parseEther("0.1"))
+        ).to.be.revertedWith("deposit not support ETH staking")
+
         await stakeProxyContract.connect(user3).deposit(1, ethers.parseEther("200"))
 
         const user1Stake = await stakeProxyContract.stakingBalance(0, user1.address)
@@ -138,6 +172,13 @@ describe("stake test", async function () {
         await stakeProxyContract.connect(user2).depositETH({ value: ethers.parseEther("20") })
 
         await stakeProxyContract.connect(user3).deposit(1, ethers.parseEther("200"))
+
+        await provider.send("evm_mine", [])
+        const blockNumber = await provider.getBlockNumber()
+        const pending = await stakeProxyContract.connect(user3).pendingMetaNodeByBlockNumber(1,user3.address,blockNumber)
+        console.log("用户可领取奖励：", pending);
+
+
         const user1StakeAgain = await stakeProxyContract.stakingBalance(0, user1.address)
         const user2StakeAgain = await stakeProxyContract.stakingBalance(0, user2.address)
         const user3StakeAgain = await stakeProxyContract.stakingBalance(1, user3.address)
@@ -148,6 +189,9 @@ describe("stake test", async function () {
     })
 
     it("unstake", async () => {
+
+        await expect(stakeProxyContract.connect(user1).unstake(0,ethers.parseEther("100"))).to.be.revertedWith("Not enough staking token balance")
+
         await stakeProxyContract.connect(user1).unstake(0, ethers.parseEther("2"))
         await stakeProxyContract.connect(user2).unstake(0, ethers.parseEther("2"))
         await stakeProxyContract.connect(user3).unstake(1, ethers.parseEther("10"))
@@ -174,6 +218,10 @@ describe("stake test", async function () {
         await stakeProxyContract.connect(user2).unstake(0, ethers.parseEther("2"))
         await stakeProxyContract.connect(user3).unstake(1, ethers.parseEther("10"))
 
+        const [requestAmount, pendingWithdrawAmount] = await stakeProxyContract.connect(user3).withdrawAmount(1, user3.address)
+        console.log("总请求金额:", requestAmount)
+        console.log("可提取金额:", pendingWithdrawAmount)
+
         console.log("第三次解质押后的区块高度：",await provider.getBlockNumber())
         // 跳过锁定区块提现
         for (let i = 0; i < 2; i++) {
@@ -191,15 +239,27 @@ describe("stake test", async function () {
         await stakeProxyContract.massUpdatePools()
     })
 
+    it("pendingMetaNode", async () =>  {
+        const user1PendNFT = await stakeProxyContract.pendingMetaNode(0, user1.address)
+        const user2PendNFT = await stakeProxyContract.pendingMetaNode(0, user2.address)
+        const user3PendNFT = await stakeProxyContract.pendingMetaNode(1, user3.address)
+        // expect(user1PendNFT).to.eq(BigInt(460))
+        // expect(user2PendNFT).to.eq(BigInt(976))
+        // expect(user3PendNFT).to.eq(BigInt(0))
+        // console.log("user1.pendingMetaNode:",user1PendNFT)
+        // console.log("user2.pendingMetaNode:",user2PendNFT)
+        // console.log("user3.pendingMetaNode:",user3PendNFT)
+    })
+
     it("withdraw", async () => {
         console.log(user1.address)
 
         const user1BalanceBefore = await provider.getBalance(user1.address)
         const user2BalanceBefore = await provider.getBalance(user2.address)
         const user3BalanceBefore = await erc20Contract.balanceOf(user3.address)
-        console.log("user1BalanceBefore::", user1BalanceBefore)
-        console.log("user2BalanceBefore::", user2BalanceBefore)
-        console.log("user3BalanceBefore::", user3BalanceBefore)
+        // console.log("user1BalanceBefore::", user1BalanceBefore)
+        // console.log("user2BalanceBefore::", user2BalanceBefore)
+        // console.log("user3BalanceBefore::", user3BalanceBefore)
 
         // 跳过锁定区块提现 
         // for (let i = 0; i < unstakeLockedBlocks; i++) {
@@ -226,9 +286,9 @@ describe("stake test", async function () {
 
 
         // eth 余额比较,有 gas, 不完全等于
-        expect(user1BalanceAfter - user1BalanceBefore).to.lt(BigInt(2E18)).gt(BigInt(1.9E18))
-        expect(user2BalanceAfter - user2BalanceBefore).to.lt(BigInt(2E18)).gt(BigInt(1.9E18))
-        expect(user3BalanceAfter - user3BalanceBefore).to.eq(BigInt(10E18))
+        expect(user1BalanceAfter - user1BalanceBefore).to.lt(BigInt(4E18)).gt(BigInt(1.9E18))
+        expect(user2BalanceAfter - user2BalanceBefore).to.lt(BigInt(4E18)).gt(BigInt(1.9E18))
+        expect(user3BalanceAfter - user3BalanceBefore).to.eq(BigInt(20E18))
     })
 
     it("claim", async () => {
